cebb78a54e830b1f2559017e7eda86479b34533e
linxiaohanimde <71820772+linxiaohanimde@users.noreply.github.com>
2026-06-23 11:18:33 +0800

    Update image_vad.m

--- image_vad.m
+++ image_vad.m
@@ -1,4 +1,4 @@
-% 读入第一个影像
+% read the sentinel-2 observations
 im = imread('D:\doctor\paper3\data\image\bf_nir.tif');
 im = im(:,:,1);
 im = double(im);
@@ -6,23 +6,20 @@ im1 = im*0.0001-0.1;
 a = im1(:);
 
 
-% 读入第二个影像
+% read the simulated images
 im2 = imread('D:\doctor\paper3\data\image\bf_sim_nir.tif');
 im2 = im2(:,:,1);
 b2 = double(im2);
 b2 = b2(:);
 
-% 误差计算
+% calculte the delta value
 
 c = abs((a - b2) ./ a) * 100;
 
 error = c;
 delta = mean(error(:));
 
-% a1 = anew(d);
-% b3 = bnew(d);
-
-% 相关性和 RMSE
+% calculate the R2 and RMSE
 r2 = corr(a, b2);
 
 rmse = sqrt(mean((a - b2).^2));
